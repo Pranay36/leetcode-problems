c846ae56112b804d5a1358bd9182ee374e14979f
Pranay Vaidya <98728524+Pranay36@users.noreply.github.com>
2023-03-18 02:00:01 +0530

    Time: 6 ms (28.86%), Space: 7.1 MB (88.62%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,43 +2,21 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& num) {
         vector<vector<int>> ans;
-        int n = num.size();
-        int x = pow(2,n);
+        int x = pow(2,num.size());
         for(int i=0;i<x;i++){
             vector<int> v;
-          
-            for(int j=0;j<n;j++){
-            
+            int q = (x-1)&i;
+            // cout<<q<<"-";
+            for(int j=0;j<num.size();j++){
+                int mask = 1<<j;
+                // cout<<mask<<" ";
                 if(i&(1<<j)){
                     v.push_back(num[j]);
                 }
             }
+            // cout<<endl;
             ans.push_back(v);
         }
         return ans;
     }
 };
-
-// class Solution {
-// public:
-//     vector<vector<int>> subsets(vector<int>& nums) {
-//         vector<vector<int>>ans;
-        
-//         int n=nums.size();
-//         int setSize = 1<<n;   // 2^n Number of subset ;
-        
-//         for(int i=0;i<setSize;i++){
-            
-//             vector<int>temp;   // for storing values in subsets
-            
-//             for(int j=0;j<n;j++){
-                
-//                 if(i & (1<<j)){ //Checking for set bits in number i;
-//                 temp.push_back(nums[j]);   // Push the value which is at founded index of setbit 
-//                 }
-//             }
-//             ans.push_back(temp);
-//         }
-//         return ans;
-//     }
-// };
