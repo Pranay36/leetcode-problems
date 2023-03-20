1953a1423272dfa7dbfca6607216753f965a762b
Pranay Vaidya <98728524+Pranay36@users.noreply.github.com>
2023-03-20 20:29:58 +0530

    Added solution - LeetHub

--- Geek Jump - GFG/geek-jump.cpp
+++ Geek Jump - GFG/geek-jump.cpp
@@ -6,23 +6,18 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int  solve(int n ,vector<int>&v, vector<int> &dp){
-      if(n==0){
-          return 0;
-      }
-      if(dp[n]!=-1){
-          return dp[n];
-      }
-      int left = solve(n-1,v,dp)+abs(v[n]-v[n-1]);
-      int right = INT_MAX;
-      if(n>1){
-          right = solve(n-2,v,dp)+abs(v[n]-v[n-2]);
-      }
-      return dp[n]=min(right,left);
-  }
     int minimumEnergy(vector<int>& v, int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n-1,v,dp);
+        vector<int> dp(n,0);
+        dp[0]=0;
+        for(int i=1;i<n;i++){
+            int fs = dp[i-1]+abs(v[i]-v[i-1]);
+            int ss = INT_MAX;
+            if(i>1){
+                ss = dp[i-2]+abs(v[i]-v[i-2]);
+            }
+            dp[i] = min(fs,ss);
+        }
+        return dp[n-1];
         }
         
     
