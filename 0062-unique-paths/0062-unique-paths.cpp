6741f28bfc809eeface8c2557421d1d751b8d20e
Pranay Vaidya <98728524+Pranay36@users.noreply.github.com>
2023-03-26 16:13:42 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (61.56%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        int count;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 &&j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                int up = 0;
+                int right =0;
+                if(i>0){
+                    up = dp[i-1][j];
+                }
+                if(j>0){
+                    right = dp[i][j-1];
+                }
+                dp[i][j] = up+right;
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    
+    }
+};
