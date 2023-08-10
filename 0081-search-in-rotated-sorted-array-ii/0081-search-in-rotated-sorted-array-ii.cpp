d5834d938ecac1bc73d87b0b4e2f0d80bb0fa51b
Pranay Vaidya <98728524+Pranay36@users.noreply.github.com>
2023-08-10 14:50:06 +0530

    Time: 3 ms (91.34%), Space: 14.1 MB (18.44%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    bool search(vector<int>& v, int t) {
-        int n = v.size();
-        int l =0;
-        int r =n-1;
+    bool search(vector<int>& num, int t) {
+        int n  = num.size();
+        int l=0;
+        int r= n-1;
+        
         while(l<=r){
             int mid = (l+r)/2;
-            if(v[mid]==t){
+            
+            if(num[mid]==t){
                 return true;
             }
-            if((v[l] == v[mid]) && (v[r] == v[mid]))
+            if((num[l] == num[mid]) && (num[r] == num[mid]))
             {
                 l++;
                 r--;
             }
-            else if(v[l]<=v[mid]){
-                if(t>=v[l] &&t<=v[mid])
-                    r=mid-1;
-                else
-                    l = mid+1;
-                
-            }
-            else{
-                if(t>=v[mid]&&t<=v[r])
-                    l=mid+1;
+            else if(num[l]<=num[mid]){
+                if(num[mid]>=t && num[l]<=t){
+                    r = mid-1;
+                }
                 else{
-                    r =mid-1;
+                    l= mid+1;
+                }
+            }else{
+                if(num[mid]<=t&& num[r]>=t){
+                    l = mid+1;
+                }else{
+                    r = mid-1;
                 }
             }
-            
-            
         }
         return false;
     }
