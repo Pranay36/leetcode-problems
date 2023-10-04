682d3930e17d7f2589582cc09dbb1e4f1d674d63
Pranay Vaidya <98728524+Pranay36@users.noreply.github.com>
2023-10-04 13:43:22 +0530

    Time: 48 ms (77.32%), Space: 31.5 MB (15.67%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    bool isfis(vector<int> weights,int mid,int days){
-        int totday=1;
-        int cur=0;
-        for(int i=0;i<weights.size();i++){
-            cur += weights[i];
-            if(cur>mid){
-                totday++;
-                cur = weights[i];
-            }
+    int shipWithinDays(vector<int>& weight, int d) {
+        int n = weight.size();
+        int l =0;
+        int r =0;
+        // sort(weight.begin(),weight.end());
+        for(int i=0;i<n;i++){
+            r +=weight[i];
+            l = max(l,weight[i]);
         }
-        return totday<=days;
-    }
-    
-    int shipWithinDays(vector<int>& weights, int days) {
-        int tot =0;
-        int mx =0;
-        for(int i=0;i<weights.size();i++){
-            tot += weights[i];
-            mx = max(mx,weights[i]);
-        }
-        int l = mx;
-        int h = tot;
+        int ans = r;
         
-        while(l<h){
-            int mid = (l+h)/2;
-            
-            if(isfis(weights,mid,days)){
-                h = mid;
+        while(l<=r){
+            int mid = (l+r)/2;
+            int k=0;
+            int count=1;
+            for(int i=0;i<n;i++){
+                k += weight[i];
+                if(k>mid){
+                    count++;  
+                    k=0;
+                    i--;
+                }
+            }
+            if(count<=d){
+                ans = min(ans,mid);
+                 r= mid-1;
             }
             else{
                 l = mid+1;
             }
-            
         }
-        return l;
+        return ans;
     }
 };
