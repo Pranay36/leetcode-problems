4d4b65a5570b2813c0fd51d984bfb4b882ea701d
Pranay Vaidya <98728524+Pranay36@users.noreply.github.com>
2023-10-05 13:42:43 +0530

    Time: 9 ms (61.98%), Space: 16.3 MB (9.38%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,28 +1,50 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& v) {
-        int n=v.size();
-        int k;
-        if(n%3==0){
-            k = n/3;
-        }else{
-            k= n/3;
-        }
-        
-        vector<int>ans;
-        map<int,int>m;
+    vector<int> majorityElement(vector<int>& num) {
+        int n = num.size();
+        int cnt1 =0;
+        int cnt2 =0;
+        int e1 = INT_MIN;
+        int e2 =INT_MIN;
         
         for(int i=0;i<n;i++){
-            m[v[i]]++;
-        }
-        
-        for(auto it: m){
-            if(it.second > k){
-                ans.push_back(it.first);
+            if(cnt1==0 && e2!=num[i]){
+                cnt1=1;
+                e1 = num[i];
+            }
+            else if(cnt2==0 && e1!=num[i]){
+                cnt2=1;
+                e2 = num[i];
+            }
+            else if(e1==num[i]){
+                cnt1++;
+            }
+            else if(e2==num[i]){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        int k=0;
+        int j=0;
+         int mini = (int)(n/3)+1;
+        for(int i=0;i<n;i++){
+            if(num[i]==e1){
+                k++;
+            }
+            if(num[i]==e2){
+                j++;
+            }
+        }
+        vector<int>v;
+        if(k>=mini)
+            v.push_back(e1);
+        if(j>=mini)
+            v.push_back(e2);
         
-        return ans;
+        return v;
         
     }
 };
