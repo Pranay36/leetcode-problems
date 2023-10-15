95e13764822d0d4121170b45444e2b05b6f5f6d9
Pranay Vaidya <98728524+Pranay36@users.noreply.github.com>
2023-10-15 16:35:20 +0530

    Added solution - LeetHub

---
+++ Normal BST to Balanced BST - GFG/normal-bst-to-balanced-bst.cpp
@@ -0,0 +1,197 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+// } Driver Code Ends
+/*Structure of the Node of the BST is as
+struct Node
+{
+	int data;
+	Node* left, *right;
+};
+*/
+
+class Solution{
+    
+    public:
+    // Your are required to complete this function
+    // function should return root of the modified BST
+    void inorder(Node* root,vector<int> &v){
+        if(root==NULL){
+            return;
+        }
+        inorder(root->left,v);
+        v.push_back(root->data);
+        inorder(root->right,v);
+        return;
+    }
+    Node* buildBalancedTree(Node* root)
+    {
+        vector<int>v;
+        inorder(root,v);
+        // for(int i=0;i<v.size();i++){
+        //     cout<<v[i]<<" ";
+        // }
+        // cout<<endl;
+        // return root;
+        int n = v.size();
+        int mid = n/2;
+        Node* node = new Node{v[mid]};
+        
+        queue<pair<Node*,pair<int,int>>> q;
+        q.push({node,{0,mid-1}});
+        q.push({node,{mid+1,n-1}});
+        while(q.size()!=0){
+            auto p = q.front();
+            q.pop();
+            Node* parent = p.first;
+            int left = p.second.first;
+            int right = p.second.second;
+            if (left <= right && parent != NULL){
+            int mid = (left+right)/2;
+            Node* kid = new Node{v[mid]};
+            if(parent->data>v[mid]){
+                parent->left = kid;
+            }
+            else{
+                parent->right = kid;
+            }
+            q.push({kid,{left,mid-1}});
+            q.push({kid,{mid+1,right}});
+        }
+            
+        }
+        return node;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+Node* insert(struct Node* node, int key){
+	if (node == NULL) return new Node(key);
+	if (key < node->data)
+		node->left = insert(node->left, key);
+	else if (key > node->data)
+		node->right = insert(node->right, key);
+	return node;
+}
+
+void preOrder(Node* node)
+{
+	if (node == NULL)return;
+	printf("%d ", node->data);
+	preOrder(node->left);
+	preOrder(node->right);
+}
+
+int height(struct Node* node) 
+{
+  if (node==NULL) 
+    return 0;
+  int lDepth = height(node->left);
+  int rDepth = height(node->right);
+  if (lDepth > rDepth) 
+    return(lDepth+1);
+  else 
+    return(rDepth+1);
+} 
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+Node* buildBalancedTree(Node* root);
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    while(t--)
+    {
+        struct Node *root = NULL;
+        int n, temp;
+        string tree;
+        getline(cin,tree);
+        root = buildTree(tree);
+        // cout<<height(root)<<endl;
+        Solution obj;
+        root = obj.buildBalancedTree(root);
+        cout<<height(root)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
