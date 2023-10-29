4670816232e440697fd4593a911b739ca93460eb
Pranay Vaidya <98728524+Pranay36@users.noreply.github.com>
2023-10-29 12:31:50 +0530

    Added solution - LeetHub

--- Check whether K-th bit is set or not - GFG/check-whether-kth-bit-is-set-or-not.cpp
+++ Check whether K-th bit is set or not - GFG/check-whether-kth-bit-is-set-or-not.cpp
@@ -15,11 +15,7 @@ class Solution
     // Function to check if Kth bit is set or not.
     bool checkKthBit(int n, int k)
     {
-        int mask = 1<<k;
-        if((n&mask)!=0){
-            return true;
-        }
-        return false;
+      return n & 1<<k;
     }
 };
 
